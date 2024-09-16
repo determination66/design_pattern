a8b7138a8c7eaac653db05a32860cea2d26d91ed
dongchuliang <2496417370@qq.com>
2024-09-17 01:38:52 +0800

    feat✨: add abstract factory

---
+++ create_type/04_abstract_factory/README.md
@@ -0,0 +1,8 @@
+# 抽象工厂
+
+https://refactoringguru.cn/design-patterns/abstract-factory
+
+
+抽象工厂生产，某个抽象产品
+
+抽象产品，可以被多实现，来降低耦合度。增加扩展性
---
+++ create_type/04_abstract_factory/absrtact_factory_test.go
@@ -0,0 +1,35 @@
+package abstractfactory
+
+func getMainAndDetail(factory DAOFactory) {
+	factory.CreateOrderMainDAO().SaveOrderMain()
+	factory.CreateOrderDetailDAO().SaveOrderDetail()
+}
+
+func ExampleRdbFactory() {
+	var factory DAOFactory
+	factory = &RDBDAOFactory{}
+	getMainAndDetail(factory)
+
+	// Output:
+	// rdb main save
+	// rdb detail save
+
+}
+
+func ExampleXmlFactory() {
+	var factory DAOFactory
+	factory = &XMLDAOFactory{}
+	getMainAndDetail(factory)
+	// Output:
+	// xml main save
+	// xml detail save
+}
+
+func ExampleAnotherFactory() {
+	var factory DAOFactory
+	factory = &AnotherFactory{}
+	getMainAndDetail(factory)
+	// Output:
+	// xml main save
+	// rdb detail save
+}
